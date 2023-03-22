43167ad39c0a483c64d1b7938b4891968169dbdb
HPCarey <hannahpcarey@gmail.com>
2023-03-22 17:34:46 +0000

    created LikeList and LikeDetail generic views

--- drf_api/comments/views.py
+++ drf_api/comments/views.py
@@ -27,4 +27,3 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
-
--- drf_api/drf_api/urls.py
+++ drf_api/drf_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('', include('profiles.urls')),
     path('', include('comments.urls')),
+    path('', include('likes.urls')),
 ]
