006a73cfe92af578170a6810e7e340aaf9f03dcd
HPCarey <hannahpcarey@gmail.com>
2023-03-23 13:33:09 +0000

    add extra fields to Posts and Profiles and add search feature

--- drf_api/profiles/views.py
+++ drf_api/profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,8 +10,22 @@ class ProfileList(generics.ListAPIView):
     List all profiles
     No Create view (post method), as profile creation handled by django signals
     """
+
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+        ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -18,5 +33,9 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+        ).order_by('-created_at')
     serializer_class = ProfileSerializer
