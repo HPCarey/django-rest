1a90300e38f330af91134846c788e8d072ebee13
HPCarey <hannahpcarey@gmail.com>
2023-03-23 13:53:56 +0000

    dd filter feature to our api

--- drf_api/posts/views.py
+++ drf_api/posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -18,7 +19,8 @@ class PostList(generics.ListCreateAPIView):
         ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter
+        filters.SearchFilter,
+        DjangoFilterBackend,
     ]
     ordering_fields = [
         'comments_count',
@@ -26,6 +28,11 @@ class PostList(generics.ListCreateAPIView):
         'likes__created_at',
     ]
     search_fields = ['owner__username', 'title']
+    filterset_fields = [
+        'owner__profile',
+        'owner__followed__owner__profile',
+        'likes__owner__profile'
+        ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
